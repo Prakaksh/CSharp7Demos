b01e89974ddd579137bdd9cbba1bacaad9405e96
unknown <hcsinfosolutions@gmail.com>
2019-11-24 12:55:37 +0530

    project setup done

---
+++ CSharp7Demos/CSharp7Demos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace CSharp7Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine();
+            ReadLine();
+        }
+    }
+}
